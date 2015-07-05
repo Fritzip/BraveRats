0fdb54f38ee1a92b729981fe938f4d49279dc38a
Maxime Sainlot <maxime.sainlot@insa-lyon.fr>
2015-07-05 12:24:59 +0200

    handling powers except 'must reveal first'. Caution possible bugs

--- src/game.py
+++ src/game.py
@@ -8,58 +8,127 @@ from player import *
 
 class Game():
     
-    def __init__(self, p1,p2):
-        self.ps = {1 : {"p" : p1, "cards" : range(8), "bonus" : [], "points" : 0, "color" : "red"},
-                   2 : {"p" : p2, "cards" : range(8), "bonus" : [], "points" : 0, "color" : "blue"}}
+    def __init__(self, p1, p2):
+        self.ps = {1 : {"p" : p1, "cards" : range(8), "card" : -1, "bonus" : {"victories" : 0, "round_pts" : 0}, "points" : 0, "color" : "red"},
+                   2 : {"p" : p2, "cards" : range(8), "card" : -1, "bonus" : {"victories" : 0, "round_pts" : 0}, "points" : 0, "color" : "blue"}}
+
+        self.cur = {"reveal_first" : 0, "diff_scr" : 0}
+
         self.l_rounds = []
-        self.m_results = np.matrix([[0,0,0,0,0,2,0,0],
-                                    [0,0,2,1,2,2,2,3],
-                                    [0,1,0,1,2,2,2,2],
-                                    [0,2,2,0,1,2,1,2],
-                                    [0,1,1,2,0,2,2,2],
-                                    [1,1,1,1,1,0,2,2],
-                                    [0,1,1,2,1,1,0,2],
-                                    [0,4,1,1,1,1,1,0]])
-        self.d_results = {0 : self.nullifie, 1 : self.update_scores, 2 : self.update_scores,
-                          3 : self.game_over, 4 : self.game_over}
-                                   
+        # self.m_results = np.matrix([[0,0,0,0,0,2,0,0],
+        #                             [0,0,2,1,2,2,2,3],
+        #                             [0,1,0,1,2,2,2,2],
+        #                             [0,2,2,0,1,2,1,2],
+        #                             [0,1,1,2,0,2,2,2],
+        #                             [1,1,1,1,1,0,2,2],
+        #                             [0,1,1,2,1,1,0,2],
+        #                             [0,4,1,1,1,1,1,0]])
+
+
+        self.m_results = np.array([[ [[ ],0],   [[ ],0],   [[2  ],0],   [[   ],0],   [[   ],0],   [[],5],   [[2  ],0],   [[],0]  ],
+                                    [ [[ ],0],   [[ ],5],   [[2  ],5],   [[2  ],5],   [[2  ],5],   [[],5],   [[2  ],5],   [[],3]  ],
+                                    [ [[1],0],   [[1],5],   [[   ],5],   [[1,2],5],   [[1,2],5],   [[],5],   [[1,2],5],   [[],2]  ],
+                                    [ [[ ],0],   [[1],5],   [[1,2],5],   [[1  ],5],   [[1,2],5],   [[],5],   [[1,2],5],   [[],2]  ],
+                                    [ [[ ],0],   [[1],5],   [[1,2],5],   [[1,2],5],   [[1,2],5],   [[],5],   [[1,2],5],   [[],2]  ],
+                                    [ [[ ],5],   [[ ],5],   [[   ],5],   [[   ],5],   [[   ],5],   [[],5],   [[   ],5],   [[],2]  ],
+                                    [ [[1],0],   [[1],5],   [[1,2],5],   [[1,2],5],   [[1,2],5],   [[],5],   [[1,2],5],   [[],2]  ],
+                                    [ [[ ],0],   [[ ],4],   [[   ],1],   [[   ],1],   [[   ],1],   [[],1],   [[   ],1],   [[],5]  ]])
+
+
+        # self.d_results = {0 : self.nullifie, 1 : self.update_scores, 2 : self.update_scores, 
+                          # 3 : self.game_over}
+
     def run(self):
+        card = []
         for round in xrange(8) :
             print "-> Round %s/8" % (round+1)
-            card_p1 = self.ps[1]["p"].choose_card(self.ps[1]["cards"])
-            card_p2 = self.ps[2]["p"].choose_card(self.ps[2]["cards"])
-            self.update_cards(card_p1,card_p2)
-            win_round = self.evaluate_round(card_p1,card_p2)
+            #TODO reveal first
+            self.ps[1]["card"] = self.ps[1]["p"].choose_card(self.ps[1]["cards"])
+            self.ps[2]["card"] = self.ps[2]["p"].choose_card(self.ps[2]["cards"])
+            self.update_cards()
+            #TODO reinitialize reveal first here
+            win_round = self.evaluate_round()
+            print "win_round = ", win_round
             win = self.ps[win_round]["p"].name if win_round != "?" else "Nullified"
-            print "(%s) %s / %s (%s) => %s" % (self.ps[1]["p"].name,card_p1,card_p2,self.ps[2]["p"].name,win)
-            print "%s : %s, %s : %s" % (self.ps[1]["p"].name,self.ps[1]["points"],self.ps[2]["p"].name,self.ps[2]["points"])
+            self.reinitialize()
+            print "(%s) %s / %s (%s) => %s" % (self.ps[1]["p"].name, self.ps[1]["card"], self.ps[2]["card"], self.ps[2]["p"].name,win)
+            print "%s : %s, %s : %s" % (self.ps[1]["p"].name, self.ps[1]["points"], self.ps[2]["p"].name, self.ps[2]["points"])
             print self.l_rounds
-            
-    def update_cards(self,card_p1,card_p2):
-        self.ps[1]["cards"].remove(card_p1)
-        self.ps[2]["cards"].remove(card_p2)   
-                 
-    def evaluate_round(self,card_p1,card_p2):
-        result = self.m_results[card_p1,card_p2]
-        win_round = self.d_results[result](result%2+(1-result%2)*2) ## TODO: Améliorer calcul
-        self.update_round(card_p1,card_p2,win_round)
+
+    def reinitialize(self):
+        for player in [1,2]:
+            self.ps[player]["bonus"]["victories"] = 0
+
+    def update_cards(self):
+        self.ps[1]["cards"].remove(self.ps[1]["card"])
+        self.ps[2]["cards"].remove(self.ps[2]["card"])
+
+    def evaluate_round(self):
+        # result = self.m_results[card_p1, card_p2]
+        self.cur["diff_scr"] = self.ps[1]["card"] + self.ps[1]["bonus"]["round_pts"] - self.ps[2]["card"] + self.ps[2]["bonus"]["round_pts"]
+
+        for player in [1,2]: self.ps[player]["bonus"]["round_pts"] = 0
+
+        result = self.m_results[self.ps[1]["card"]][self.ps[2]["card"]]
+        for i in result[0]: self.apply_power(i)
+
+        return self.get_winner(result[1])
+
+    def get_winner(self, val):
+        if val == 0:
+            win_round = self.nullifie()
+        elif val <= 2:
+            win_round = self.update_scores(val)
+        elif val <= 4:
+            win_round = self.game_over(val - 2)
+        elif val == 5:
+            if self.cur["diff_scr"] != 0: win_round = self.update_scores(((np.sign(-self.cur["diff_scr"])+1)/2)+1)
+            else: win_round = self.nullifie()
+        else:
+            print "C'est impossible !!"
+       
+        # if result[1] == 5 :
+
+        #     win_round = self.d_results[win_round](win_round)
+        # else :
+        #     win_round = self.d_results[result[1]](result[1])
+        #     # win_round = self.d_results[result[1]](result%2+(1-result%2)*2) ## TODO: Améliorer calcul
+
+        self.update_round(win_round)
         return win_round
-        
-    def update_scores(self,player):
+
+    def apply_power(self, player):
+        if self.ps[player]["card"] == 2: # reveal card
+            self.cur["reveal_first"] = (player%2)+1
+            print "%s must reveal first next round"% self.ps[(player%2)+1]["p"].name
+        elif self.ps[player]["card"] == 3: # lowest wins
+            self.cur["diff_scr"] = -self.cur["diff_scr"]
+            print "Lowest wins"
+        elif self.ps[player]["card"] == 4: # +2 victories
+            self.ps[player]["bonus"]["victories"] = 1
+            print "Count 2 victories if you win : %s"% self.ps[player]["p"].name
+        elif self.ps[player]["card"] == 6: # +2 round pts next round
+            self.ps[player]["bonus"]["round_pts"] = 2
+            print "+2 next round for %s"% self.ps[player]["p"].name
+        else: 
+            print "C'est impossible !!"
+
+    def update_scores(self, player):
         if self.l_rounds and self.l_rounds[-1][-1] == "?" : 
             self.l_rounds[-1][-1] = player
             self.ps[player]["points"] += 1
-        self.ps[player]["points"] += 1
+        self.ps[player]["points"] += 1 + self.ps[player]["bonus"]["victories"]
         if self.ps[player]["points"] >= 4 : self.game_over(player)
         else : return player
     
-    def nullifie(self,val):
+    def nullifie(self):
         return "?"
-               
-    def update_round(self,card_p1,card_p2,win):
-        self.l_rounds.append([(card_p1,card_p2),win])
+
+    def update_round(self, win):
+        self.l_rounds.append([(self.ps[1]["card"], self.ps[2]["card"]), win])
         
-    def game_over(self,player):
+    def game_over(self, player):
+        # if self.ps[1]["card"] == 7: player = (player%2)+1 # HERE !
         print "Game Over : %s win !!!" % self.ps[player]["p"].name
         sys.exit()
         
--- src/player.py
+++ src/player.py
@@ -1,15 +1,30 @@
 #! /usr/bin/python2
 # -*- coding: utf-8 -*-
 
-CARDS = ["Magicien","Princesse","Espion","Assassin","Ambassadeur","Sorcier","General","Prince"]
-POWERS = ["The round is nullified","Against the prince you win","Next round your opponents reveal his card","The lowest streigth wins","This round count 2 victories if you win it","Nullified opponent's card power","+2 for your next round","You win the round"]
+CARDS = ["Magicien",
+        "Princesse",
+        "Espion",
+        "Assassin",
+        "Ambassadeur",
+        "Sorcier",
+        "General",
+        "Prince"]
+
+POWERS = ["The round is nullified", # handled by matrix
+        "Against the prince you win",#handled by matrix
+        "Next round your opponents reveal his card", # todo
+        "The lowest streigth wins",#handled by matrix
+        "This round count 2 victories if you win it", # in bonus
+        "Nullified opponent's card power",#no apply power for other player
+        "+2 for your next round", # in bonus
+        "You win the round"] #handled by matrix
 
 class Player():
     
     def __init__(self, name):
         self.name = name
       
-    def choose_card(self,cards) :
+    def choose_card(self, cards):
         print "%s, choose a card to play" % self.name
         for c in cards : print "- (%s) %s (%s)" %(c,CARDS[c],POWERS[c])
         #while True :
